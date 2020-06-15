eba10282fa1a22fbe4f0af889bedf58d310c24a6
45gfg9 <mc45@qq.com>
2020-06-16 00:03:14 +0800

    Get weather info

--- src/d1_central.cpp
+++ src/d1_central.cpp
@@ -11,61 +11,68 @@ SoftwareSerial ss(2, 3);
 ESP8266WebServer server(80);
 
 void handleSerial(Stream &serial);
-char psk[18] = {};
+char psk[PSK_STRLEN] = {};
 
 void setup()
 {
   delay(1000);
-  Serial.begin(115200);
   ss.begin(115200);
+  Serial.begin(115200);
 
-  EEPROM.begin(18);
+  EEPROM.begin(PSK_STRLEN);
   for (int i = 0; i < PSK_STRLEN; i++)
     psk[i] = EEPROM.read(i);
   EEPROM.end();
+  Serial.println();
+  Serial.println(psk);
 
   IPAddress ip = remote::begin();
   Serial.println(ip);
 
   server.on("/weather", []() {
-    const uint8_t url_len = 100;
-    const char *host = PSTR("api.seniverse.com");
-    const char *uri = PSTR("/v3/weather/now.json");
-    const char *incompleteQuery = PSTR("?location=WQ7SF3WEPQRU&key=");
-    char url[url_len];
-    snprintf_P(url, url_len, PSTR("%s%s%s%s"), host, uri, incompleteQuery, psk);
+    const char *host = ("api.seniverse.com");
+    const char *uri = ("/v3/weather/now.json");
+    const char *partialQuery = ("?location=WQ7SF3WEPQRU&key=");
 
     WiFiClient client;
-    if (!client.connect(url, 443))
+    if (!client.connect(host, 80))
     {
       server.send(500, "text/plain", "connection failed");
       client.stop();
       return;
     }
 
-    if (client.connected())
-    {
-      client.printf_P(PSTR("GET %s%s%s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n"), uri, incompleteQuery, psk, host);
-    }
+    client.printf_P(PSTR("GET %s%s%s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n"), uri, partialQuery, psk, host);
 
     unsigned long timeout = millis();
     while (client.available() == 0)
     {
       if (millis() - timeout > 5000)
       {
-        server.send(504, "text/plain", "connection timed out");
+        server.send(504, "text/plain", "Connection timed out");
         client.stop();
         return;
       }
     }
+    delay(500);
 
     const int available = client.available();
     char data[available];
-    for (int i = 0; i < available; i++)
+    int i = 0;
+    bool read = false;
+    while (client.available())
     {
-      data[i] = client.read();
+      char tmp;
+      if ((tmp = client.read()) == '{' || read)
+      {
+        read = true;
+        data[i++] = tmp;
+      }
     }
+    data[i] = 0;
 
+    Serial.println(data);
+    ss.println(data);
     server.send(200, "text/plain", data);
   });
   server.begin();
@@ -75,13 +82,12 @@ void loop()
 {
   server.handleClient(); // non-blocking
 
-  handleSerial(ss);
+  // handleSerial(ss);
 }
 
 void handleSerial(Stream &serial)
 {
   while (serial.available())
   {
-    serial.read();
   }
 }
