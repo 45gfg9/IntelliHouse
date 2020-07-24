1bb0fe0f2414517620c8a170ce75f55262dd08f5
45gfg9 <mc45@qq.com>
2020-07-24 11:43:52 +0800

    Setup env

--- src/d1_env.cpp
+++ src/d1_env.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
+#include <WiFiUdp.h>
 #include <Adafruit_NeoPixel.h>
+#include <DateTime.h>
 #include "remote.hxx"
+
+#define LED_COUNT 30
+#define LED_PIN D0
 
 /*
 Time
@@ -16,14 +21,23 @@ on
 off (but manual)
 */
 
+WiFiUDP udp;
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN);
+
 void setup()
 {
     Serial.begin(BAUD_RATE);
     Serial.println();
 
+    strip.begin();
+    strip.show();
+
     remote::connect();
+
+    udp.begin(UDP_PORT);
 }
 
 void loop()
 {
+    remote::listenTime(udp);
 }
